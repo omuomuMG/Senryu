3e60189ccb61a2bcab905458eee817965ee5faaf
omuomuMG <omuomunotrice@gmail.com>
2024-06-16 17:29:23 +0900

    feature / Edit

---
+++ app/EditContent/[id]/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { doc, setDoc, updateDoc } from "firebase/firestore";
+import { useRef } from "react";
+import db from "../../Firebase";
+import toast, { Toaster } from "react-hot-toast";
+
+type Content = {
+	id: string;
+	title: string | undefined;
+	body: string | undefined;
+	rating: number;
+};
+
+export default function EditContent() {
+	const titleRef = useRef<HTMLInputElement | null>(null);
+	const bodyRef = useRef<HTMLTextAreaElement | null>(null);
+	const id = location.pathname.replace(/\/+$/, "").split("/").pop();
+	const handleSubmit = async (e: React.FormEvent) => {
+		e.preventDefault();
+		toast.loading("本棚に収納中", {
+			duration: 3000,
+		});
+		const newContent: Content = {
+			body: bodyRef?.current?.value,
+			id: Math.random().toString(32).substring(2),
+			title: titleRef?.current?.value,
+			rating: 4,
+		};
+		await updateDoc(doc(db, "user1"), newContent);
+		toast.success("本棚に収納しました！", {
+			duration: 3000,
+		});
+	};
+	return (
+		<>
+			<div>
+				<h1>{id}</h1>
+				<Toaster />
+				<h1>本を記録</h1>
+				<form onSubmit={handleSubmit}>
+					<label>
+						本の名前:
+						<input type="text" name="title" ref={titleRef} />
+						感想:
+						<textarea
+							ref={bodyRef}
+							placeholder="記事詳細を入力"
+							className="rounded-md px-4 py-2 w-full my-2"
+						></textarea>
+					</label>
+					<input type="submit" value="Submit" />
+				</form>
+			</div>
+		</>
+	);
+}
