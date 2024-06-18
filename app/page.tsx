ba6b9029e44d3b4c2a9de814bb8f558b33afdb81
omuomuMG <omuomunotrice@gmail.com>
2024-06-18 19:13:01 +0900

    feature / zustand

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { useEffect, useState } from "react";
-import { collection, getDoc, getDocs } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import Link from "next/link";
 import db from "./Firebase";
+import { create } from "zustand";
 
 type Content = {
 	id: string;
@@ -11,46 +12,65 @@ type Content = {
 	rating: number;
 };
 
+type ContentState = {
+	id: string;
+	title: string;
+	body: string;
+	rating: number;
+	getContent: (content: Content) => void;
+};
+export const useStore = create<ContentState>((set) => ({
+	id: "",
+	title: "",
+	body: "",
+	rating: 0,
+	getContent: (content: Content) =>
+		set({
+			id: content.id,
+			title: content.title,
+			body: content.body,
+			rating: content.rating,
+		}),
+	feachContet: set((state) => ({})),
+}));
+
 export default function Home() {
-	const [Contents, setContents] = useState<Content[]>([]);
+	const [contents, setContents] = useState<Content[]>([]);
+
 	useEffect(() => {
 		fetchContents();
 	}, []);
+
 	const fetchContents = async () => {
 		try {
 			const contentsCollectionRef = collection(db, "user1");
 			const querySnapShot = await getDocs(contentsCollectionRef);
 			const contentsData: Content[] = [];
 			querySnapShot.forEach((doc) => {
-				contentsData.push(doc.data() as Content);
+				contentsData.push({ id: doc.id, ...doc.data() } as Content);
 			});
 			setContents(contentsData);
 		} catch (e) {
-			console.log("Error featching contents", e);
+			console.log("Error fetching contents", e);
 		}
 	};
+
+	const getContent = useStore((state) => state.getContent);
+
 	return (
 		<>
 			<div>
-				<Link href="/PostContent/">a</Link>
+				<Link href={{ pathname: "/PostContent" }}>a</Link>
 				<h1>Content</h1>
-				{Contents.map((content) => (
-					<>
-						<div>
-							<h2>{content.title}</h2>
-							<p>{content.body}</p>
-							<p>{content.rating}</p>
-						</div>
-						<Link
-							href={{
-								pathname: `/EditContent/${content.id}}`,
-								query: {
-									id: content.id,
-									name: content.body,
-								},
-							}}
-						></Link>
-					</>
+				{contents.map((content) => (
+					<div key={content.id}>
+						<h2>{content.title}</h2>
+						<p>{content.body}</p>
+						<p>{content.rating}</p>
+						<Link href={`/EditContent/${content.id}`}>
+							<button onClick={() => getContent(content)}>Edit</button>
+						</Link>
+					</div>
 				))}
 			</div>
 		</>
--- src/pages/EditContent/[id]/page.tsx
+++ src/pages/EditContent/[id]/page.tsx
@@ -10,9 +10,7 @@ import {
 import { useEffect, useRef, useState } from "react";
 import db from "../../Firebase";
 import toast, { Toaster } from "react-hot-toast";
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
-import { useStore } from "@/app/page";
+import { useRouter } from "next/navigation";
 
 type Content = {
 	id: string;
@@ -24,20 +22,39 @@ type Content = {
 export default function EditContent() {
 	const titleRef = useRef<HTMLInputElement | null>(null);
 	const bodyRef = useRef<HTMLTextAreaElement | null>(null);
+	const contentId = location.pathname.replace(/\/+$/, "").split("/").pop();
 	const [content, setContent] = useState<Content>();
-
+	const router = useRouter();
 	let getContent: Content = {
 		id: "",
 		title: undefined,
 		body: undefined,
 		rating: 0,
 	};
-	const { id, title, body, rating } = useStore((state) => ({
-		id: state.id,
-		title: state.title,
-		body: state.body,
-		rating: state.rating,
-	}));
+
+	useEffect(() => {
+		fetchContent();
+	}, []);
+
+	const fetchContent = async () => {
+		try {
+			console.log(contentId);
+			const contentCollectionRef = collection(db, "user1");
+
+			const querySnapShot = query(
+				contentCollectionRef,
+				where("id", "==", contentId)
+			);
+
+			getContent = await getDocs(querySnapShot).then((snapshot) => {
+				return snapshot.docs[0].data() as Content;
+			});
+			setContent(getContent);
+			console.log(content);
+		} catch (e) {
+			console.log("Error featching contents", e);
+		}
+	};
 
 	const handleSubmit = async (e: React.FormEvent) => {
 		e.preventDefault();
@@ -59,20 +76,25 @@ export default function EditContent() {
 	return (
 		<>
 			<div>
-				<h1>{id}</h1>
-				<h1>{body}</h1>
+				<h1>{contentId}</h1>
+				<h1>{content?.body}</h1>
 				<Toaster />
 				<h1>本を記録</h1>
 				<form onSubmit={handleSubmit}>
 					<label>
 						本の名前:
-						<input type="text" name="title" ref={titleRef} value={title} />
+						<input
+							type="text"
+							name="title"
+							ref={titleRef}
+							value={content?.title}
+						/>
 						感想:
 						<textarea
 							ref={bodyRef}
 							placeholder="記事詳細を入力"
 							className="rounded-md px-4 py-2 w-full my-2"
-							value={body}
+							value={content?.body}
 						></textarea>
 					</label>
 					<input type="submit" value="Submit" />
