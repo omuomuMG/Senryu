8c183d71545a73469fe168278963ffa9420985e9
omuomuMG <omuomunotrice@gmail.com>
2024-10-01 18:44:57 +0900

    Refact / Update

--- app/EditContent/page.tsx
+++ app/EditContent/page.tsx
@@ -9,14 +9,24 @@ import { useRouter } from "next/navigation";
 export default function EditContent() {
   const titleRef = useRef<HTMLTextAreaElement | null>(null);
   const bodyRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const firstPartRef = useRef<HTMLTextAreaElement | null>(null);
+  const midlePartRef = useRef<HTMLTextAreaElement | null>(null);
+  const lastPartRef = useRef<HTMLTextAreaElement | null>(null);
+
   const router = useRouter();
 
-  const { id, title, body, rating } = useStore((state) => ({
-    id: state.id,
-    title: state.title,
-    body: state.body,
-    rating: state.rating,
-  }));
+  const { id, title, body, rating, firstPart, midlePart, lastPart } = useStore(
+    (state) => ({
+      id: state.id,
+      title: state.title,
+      body: state.body,
+      rating: state.rating,
+      firstPart: state.firstPart,
+      midlePart: state.midlePart,
+      lastPart: state.lastPart,
+    })
+  );
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -28,11 +38,14 @@ export default function EditContent() {
       id: id,
       title: titleRef?.current?.value,
       rating: 4,
-      firstPart: "",
-      midlePart: "",
-      lastPart: "",
+      firstPart: firstPartRef?.current?.value,
+      midlePart: midlePartRef?.current?.value,
+      lastPart: lastPartRef?.current?.value,
     };
-    await updateDoc(doc(db, "user1", newContent?.id as string), newContent);
+    await updateDoc(
+      doc(db, "all", "user01", "poem1", newContent?.id as string),
+      newContent
+    );
     toast.success("本を書き換えました！", {
       duration: 3000,
     });
@@ -45,6 +58,7 @@ export default function EditContent() {
       <div>
         <h1>{id}</h1>
         <h1>{body}</h1>
+        <h1>{firstPart}</h1>
         <Toaster />
         <h1>本を記録</h1>
         <form onSubmit={handleSubmit}>
@@ -62,8 +76,30 @@ export default function EditContent() {
               ref={bodyRef}
               placeholder="記事詳細を入力"
               className="rounded-md px-4 py-2 w-full my-2"
+            ></textarea>
+            上:
+            <textarea
+              ref={firstPartRef}
+              placeholder="記事詳細を入力"
+              className="rounded-md px-4 py-2 w-full my-2"
             >
-              {body}
+              {firstPart}
+            </textarea>
+            中:
+            <textarea
+              ref={midlePartRef}
+              placeholder="記事詳細を入力"
+              className="rounded-md px-4 py-2 w-full my-2"
+            >
+              {midlePart}
+            </textarea>
+            下:
+            <textarea
+              ref={lastPartRef}
+              placeholder="記事詳細を入力"
+              className="rounded-md px-4 py-2 w-full my-2"
+            >
+              {lastPart}
             </textarea>
           </label>
           <input type="submit" value="Submit" />
