6049045940ef204bcfc61f4c2b55d4da9fac78f1
omuomuMG <omuomunotrice@gmail.com>
2025-02-21 17:36:30 +0900

    Layout: SignOut design

--- app/Logout/page.tsx
+++ app/Logout/page.tsx
@@ -2,14 +2,25 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../Firebase";
 import { useEffect } from "react";
+import styles from "./SignOut.module.css";
+import Link from "next/link";
 
 export default function SignOut() {
   const [user] = useAuthState(auth);
 
   return (
-    <div>
-      <button onClick={() => auth.signOut()}>サインアウト</button>
-      <div>{user ? <p>ろぐいんんずむ</p> : <p>ずむくない</p>}</div>;
+    <div className={styles.container}>
+      <button className={styles.signOutButton} onClick={() => auth.signOut()}>
+        <span className={styles.buttonText}>ログアウトされました</span>
+      </button>
+      <div className={styles.statusContainer}>
+        {user ? (
+          <p className={styles.statusText}>ログイン中</p>
+        ) : (
+          // <p className={styles.statusText}>ログアウトされました</p>
+          <Link href={{ pathname: "/" }}>ホームへ戻る</Link>
+        )}
+      </div>
     </div>
   );
 }
