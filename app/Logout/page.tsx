0e4ea46f91c910ec4d3a44f5090ef35832c88967
omuomuMG <omuomunotrice@gmail.com>
2025-02-22 21:08:51 +0900

    Layout: Add Header to SingOut Page

--- app/Logout/page.tsx
+++ app/Logout/page.tsx
@@ -4,21 +4,24 @@ import { auth } from "../Firebase";
 import { useEffect } from "react";
 import styles from "./SignOut.module.css";
 import Link from "next/link";
+import Header from "../Header/header";
 
 export default function SignOut() {
   const [user] = useAuthState(auth);
-
   return (
-    <div className={styles.container}>
-      <button className={styles.signOutButton} onClick={() => auth.signOut()}>
-        <span className={styles.buttonText}>ログアウトされました</span>
-      </button>
-      <div className={styles.statusContainer}>
-        {user ? (
-          <p className={styles.statusText}>ログイン中</p>
-        ) : (
-          <Link href={{ pathname: "/" }}>ホームへ戻る</Link>
-        )}
+    <div>
+      <Header />
+      <div className={styles.container}>
+        <button className={styles.signOutButton} onClick={() => auth.signOut()}>
+          <span className={styles.buttonText}>ログアウトされました</span>
+        </button>
+        <div className={styles.statusContainer}>
+          {user ? (
+            <p className={styles.statusText}>ログイン中</p>
+          ) : (
+            <Link href={{ pathname: "/" }}>ホームへ戻る</Link>
+          )}
+        </div>
       </div>
     </div>
   );
