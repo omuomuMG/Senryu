86df215066959e19e3074236af8fa79bdd4a3eb9
omuomuMG <omuomunotrice@gmail.com>
2025-05-11 00:01:00 +0900

    Add: Deploy info

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = { output: "export" };
+const nextConfig = {
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/Senryu" : "",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
