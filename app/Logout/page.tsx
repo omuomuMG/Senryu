6badc132222202e75d5e0a806dfbd123d667d318
omuomuMG <omuomunotrice@gmail.com>
2025-05-11 15:53:41 +0900

    Feautre: ログアウトが成功後リダイレクト

--- app/Logout/page.tsx
+++ app/Logout/page.tsx
@@ -3,25 +3,31 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../Firebase";
 import { useEffect } from "react";
 import styles from "./SignOut.module.css";
-import Link from "next/link";
 import Header from "../Header/header";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 export default function SignOut() {
   const [user] = useAuthState(auth);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      toast.loading("リダイレクトします", { duration: 3000 });
+      toast.success("成功しました", { duration: 3000 });
+      router.push("/");
+      router.refresh();
+    }
+  }, [user, router]);
+
   return (
     <div>
       <Header />
       <div className={styles.container}>
         <button className={styles.signOutButton} onClick={() => auth.signOut()}>
-          <span className={styles.buttonText}>ログアウトされました</span>
+          <span className={styles.buttonText}>ログアウトする</span>
         </button>
-        <div className={styles.statusContainer}>
-          {user ? (
-            <p className={styles.statusText}>ログイン中</p>
-          ) : (
-            <Link href={{ pathname: "/" }}>ホームへ戻る</Link>
-          )}
-        </div>
+        <div className={styles.statusContainer}></div>
       </div>
     </div>
   );
