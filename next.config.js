e8a47518dac2801aaac061c02724fb5d18445a89
omuomuMG <omuomunotrice@gmail.com>
2025-05-11 13:31:06 +0900

    FIx: deploybug

--- next.config.js
+++ next.config.js
@@ -3,4 +3,10 @@ const nextConfig = {
   output: "export",
 };
 
+module.exports = {
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+  trailingSlash: true,
+};
+
 module.exports = nextConfig;
